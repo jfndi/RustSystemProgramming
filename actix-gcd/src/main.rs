1ad79e02164bd6cc4b07753bf1a5527f7425a101
jfndi <143571177+jfndi@users.noreply.github.com>
2023-12-16 17:45:35 +0100

    Add actix-gcd first iteration.

---
+++ actix-gcd/src/main.rs
@@ -0,0 +1,37 @@
+/*
+ * @Author: Jean-François Ndi 
+ * @Date: 2023-12-16 17:08:30 
+ * @Last Modified by: Jean-François Ndi
+ * @Last Modified time: 2023-12-16 17:10:44
+ */
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+fn main() {
+    let server = HttpServer::new(
+        || 
+        {
+            App::new()
+                .route("/", web::get().to(get_index))
+        });
+
+    println!("Serving on http://localhost:3000...");
+    server
+        .bind("127.0.0.1:3000").expect("error binding server to address")
+        .run().expect("error running server");
+}
+
+fn get_index() -> HttpResponse 
+{
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
+            "#,
+        )
+}
