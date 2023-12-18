3cc03ded20c85121c2278349415240bba07d116b
jfndi <143571177+jfndi@users.noreply.github.com>
2023-12-18 15:40:14 -0500

    Add the POST treatment in actix-gcd.

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -5,6 +5,31 @@
  * @Last Modified time: 2023-12-16 17:10:44
  */
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters
+{
+    n: u64,
+    m: u64
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64
+{
+    assert!(n!= 0 && m != 0);
+
+    while m != 0
+    {
+        if m < n
+        {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
 
 fn main() {
     let server = HttpServer::new(
@@ -12,6 +37,7 @@ fn main() {
         {
             App::new()
                 .route("/", web::get().to(get_index))
+                .route("/gcd", web::post().to(post_gcd))
         });
 
     println!("Serving on http://localhost:3000...");
@@ -34,4 +60,22 @@ fn get_index() -> HttpResponse
                 </form>
             "#,
         )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse
+{
+    if form.n == 0 || form.m == 0
+    {
+        return HttpResponse::BadRequest()
+                .content_type("text/html")
+                .body("Computing the GCD with zero is boring.");
+    }
+
+    let response =
+            format!("The greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
+                     form.n, form.m, gcd(form.n, form.m));
+    
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
